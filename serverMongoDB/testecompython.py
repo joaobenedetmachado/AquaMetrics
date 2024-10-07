4951dccf9b730b3d6005a584eac0aa73c7f9da3e
joao victor machado <jvbm2177@gmail.com>
2024-10-07 16:29:12 -0300

    conexao com mongoDB

---
+++ serverMongoDB/testecompython.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+minhaSenha = '123'
+client = MongoClient(f"mongodb+srv://joaoteste:{minhaSenha}@iotcluster.a70ey.mongodb.net/?retryWrites=true&w=majority&appName=IotCluster")
+
+db = client["aqua"]
+collection = db["aquacollections"]
+
+date = ''
+ph = ''
+tds = ''
+temp = ''
+
+documento = {
+    "data": date,
+    "ph": ph,
+    "tds": tds,
+    "temp" : temp
+}
+
+
+resultado = collection.insert_one(documento)
+
+print(f"Documento inserido com ID: {resultado.inserted_id}")
