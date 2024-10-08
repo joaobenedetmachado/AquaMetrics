ca5f6260e6c300163dc9897796ef9f45a275e613
joao machado <jvbm2177@gmail.com>
2024-10-08 10:24:49 -0300

    mudei os teste

--- testeESPcomAPI/server.py
+++ testeESPcomAPI/server.py
@@ -1,17 +1,18 @@
-from flask import Flask, request
-from pymongo import MongoClient
+import requests
+from datetime import datetime
 
-app = Flask(__name__)
+# URL da API
+url = "https://aqua-metrics-api.vercel.app/documentos"  # Substitua pela URL da sua API
 
-client = MongoClient("mongodb+srv://joaoteste:123@iotcluster.a70ey.mongodb.net/?retryWrites=true&w=majority&appName=IotCluster")
-db = client["aqua"]
-collection = db["aquacollections"]
+# Enviando os dados via POST para a API
+enviar = requests.post(url, json={
+    "data" : "2021-10-07 10:00:00",
+    "ph" : "7",
+    "tds" : "20",
+    "temp" : "20"
+})
 
-@app.route('/insertdata', methods=['POST'])
-def insert_data():
-    data = request.json
-    collection.insert_one(data)
-    return {"message": "Dados inseridos com sucesso!"}, 200
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if enviar.status_code == 200:
+    print("deu certo")
+else:
+    print("deu errado")
