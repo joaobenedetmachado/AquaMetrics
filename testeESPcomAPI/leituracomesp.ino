e560a693ac740ea6176028c8f6d583a1a051cf02
João Benedet <105388117+joaobenedetmachado@users.noreply.github.com>
2024-11-04 16:54:02 -0300

    Update leituracomesp.ino

--- testeESPcomAPI/leituracomesp.ino
+++ testeESPcomAPI/leituracomesp.ino
@@ -2,18 +2,27 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <HTTPClient.h>
-#include <WiFi.h>  // Adiciona a biblioteca WiFi para ESP32
+#include <WiFi.h>
+#include <time.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128  // Largura da tela OLED
+#define SCREEN_HEIGHT 64  // Altura da tela OLED
+#define OLED_RESET -1     // Reset pin (não é necessário para a maioria dos displays OLED)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const char* ssid = "SATC IOT";
 const char* password = "IOT2024@#";
 const char* serverName = "https://aquametrics-api.vercel.app/documentos";
 
-#define PH_PIN 35              // Pino analógico para o sensor de pH
-#define TEMPERATURE_PIN 2      // Pino de entrada para o sensor de temperatura
+#define PH_PIN 35          // Pino analógico para o sensor de pH
+#define TEMPERATURE_PIN 2  // Pino de entrada para o sensor de temperatura
 
-const float VOLTAGE_REF = 3.3;    
-const float OFFSET = 0.4; // Calibracao para a correcao do valor
-const int SAMPLES = 10;     
+const float VOLTAGE_REF = 3.3;
+const float OFFSET = 0.4;  // Calibracao para a correcao do valor
+const int SAMPLES = 10;
 
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
@@ -21,29 +30,27 @@ DallasTemperature sensors(&oneWire);
 // Função para ler o pH
 float LeituraPH() {
   float adc_value = 0;
-  
-  for(int i = 0; i < SAMPLES; i++) {
+  for (int i = 0; i < SAMPLES; i++) {
     adc_value += analogRead(PH_PIN);
     delay(10);
   }
   adc_value = adc_value / SAMPLES;
-  
+
   float voltage = (adc_value * VOLTAGE_REF) / 4095.0;
   float ph_value = 3.5 * voltage + OFFSET;
-  
+
   return ph_value;
 }
 
 // Função para ler a temperatura
 float LeituraTemperatura() {
-  sensors.requestTemperatures();             // Solicita a leitura de temperatura do sensor
-  return sensors.getTempCByIndex(0);         // Retorna a temperatura em Celsius
+  sensors.requestTemperatures();      // Solicita a leitura de temperatura do sensor
+  return sensors.getTempCByIndex(0);  // Retorna a temperatura em Celsius
 }
 
 void setup() {
-  
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA); // Define explicitamente o modo Station
+  WiFi.mode(WIFI_STA);  // Define explicitamente o modo Station
   WiFi.begin(ssid, password);
 
   Serial.print("Conectando à rede Wi-Fi");
@@ -62,10 +69,22 @@ void setup() {
     Serial.println("\nFalha na conexão Wi-Fi!");
   }
 
-  analogReadResolution(12);  // Configura a resolução da leitura analógica
-  Serial.println("Sistema iniciado!");
-  
-  sensors.begin();  // Inicializa o sensor de temperatura
+  analogReadResolution(12);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Endereço I2C padrão: 0x3C
+    Serial.println(F("Erro ao inicializar o display OLED"));
+    for (;;)
+      ;
+  }
+  display.clearDisplay();
+  display.setTextSize(1);               // Tamanho do texto
+  display.setTextColor(SSD1306_WHITE);  // Cor do texto
+ // Configura a resolução da leitura analógica
+Serial.println("Sistema iniciado!");
+
+sensors.begin();  // Inicializa o sensor de temperatura
+
+// Configura o fuso horário para Brasília (UTC-3)
+configTime((-3 * 3600), 0, "pool.ntp.org");
 }
 
 void loop() {
@@ -74,7 +93,7 @@ void loop() {
     WiFi.disconnect();
     WiFi.reconnect();
     delay(1000);
-  }  
+  }
 
   float ph = LeituraPH();
   Serial.print("pH: ");
@@ -87,7 +106,16 @@ void loop() {
   delay(1000);
 
   if (WiFi.status() == WL_CONNECTED) {
-    String dataHora = "2024-03-21 14:00:00";  
+    // Obtém a data e hora atual em Brasília
+    time_t now;
+    struct tm* localTime;
+    time(&now);
+    localTime = localtime(&now);
+
+    char dataHora[20];
+    sprintf(dataHora, "%04d-%02d-%02d %02d:%02d:%02d",
+            localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday,
+            localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
 
     HTTPClient http;
     http.setConnectTimeout(1000);
@@ -95,10 +123,12 @@ void loop() {
     http.begin(serverName);
     http.addHeader("Content-Type", "application/json");
 
-    String jsonPayload = "{\"data\": \"" + dataHora + "\", "
-                        "\"ph\": " + String(ph, 2) + ", "
-                        "\"tds\": 1200.2, "
-                        "\"temp\": " + String(temperatura, 2) + "}";
+    String jsonPayload = "{\"data\": \"" + String(dataHora) + "\", "
+                                                              "\"ph\": "
+                         + String(ph, 2) + ", "
+                                           "\"tds\": 1200.2, "
+                                           "\"temp\": "
+                         + String(temperatura, 2) + "}";
 
     Serial.println("Enviando dados: " + jsonPayload);
 
@@ -115,6 +145,14 @@ void loop() {
     }
 
     http.end();
-    delay(1000); 
+    delay(1000);
   }
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("pH: ");
+  display.println(ph, 2);
+  display.print("Temperatura: ");
+  display.println(temperatura, 2);
+  display.display();
+  delay(1000);
 }
